0c5a721215693bbbcb18c51a9931f9bc0830254e
dimaswisnuprabowo <kelvinadinata82@gmail.com>
2024-07-04 13:34:38 +0700

    feat: predict linking-domains

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask, request, jsonify
 import joblib
+from flask import Flask, request, jsonify
 import numpy as np
-
 
 app=Flask(__name__)
 
 model1 = joblib.load('my_model1.joblib')
 model2 = joblib.load('my_model2.joblib')
+
+@app.route('/')
+def index():
+    return '<h1>Hello World</h1>'
 
 @app.route('/predict-backlinks', methods=['POST'])
 def predict() :
@@ -16,5 +19,12 @@ def predict() :
     output = prediction.tolist()
     return jsonify({'prediction ' : output })
 
-if __name__ == '__main__':
-    app.run(debug=true)
+@app.route('/predict-linking-domains', methods=['POST'])
+def predict_link() :
+    data = request.get_json(force=True)
+    input_data = np.array(data['input'])
+    prediction = model2.predict(input_data)
+    output = prediction.tolist()
+    return jsonify({'prediction ' : output })
+
+app.run()
